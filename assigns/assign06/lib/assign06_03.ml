a1d0637467556e864e97c10e1f953f46b0b401ab
Ethanyoy <119466937+Ethanyoy@users.noreply.github.com>
2024-10-23 22:06:12 -0400

    Update assign06_03.ml

--- assigns/assign06/lib/assign06_03.ml
+++ assigns/assign06/lib/assign06_03.ml
@@ -1,25 +1,15 @@
-type ty = 
-  | TInt
-  | TBool
-
-type expr = 
-  | Num of int
-  | Add of expr * expr
-  | Lt of expr * expr
-  | Ite of expr * expr * expr
-
-let rec type_of (e: expr) : ty option =
+let rec type_of (e: Utils.expr) : Utils.ty option =
   match e with
-  | Num _ -> Some TInt
-  | Add(e1, e2) ->
+  | Utils.Num _ -> Some Utils.TInt
+  | Utils.Add(e1, e2) ->
     (match (type_of e1, type_of e2) with
-     | (Some TInt, Some TInt) -> Some TInt
+     | (Some Utils.TInt, Some Utils.TInt) -> Some Utils.TInt
      | _ -> None)
-  | Lt(e1, e2) ->
+  | Utils.Lt(e1, e2) ->
     (match (type_of e1, type_of e2) with
-     | (Some TInt, Some TInt) -> Some TBool
+     | (Some Utils.TInt, Some Utils.TInt) -> Some Utils.TBool
      | _ -> None)
-  | Ite(e1, e2, e3) ->
+  | Utils.Ite(e1, e2, e3) ->
     (match (type_of e1, type_of e2, type_of e3) with
-     | (Some TBool, Some t2, Some t3) when t2 = t3 -> Some t2
+     | (Some Utils.TBool, Some t2, Some t3) when t2 = t3 -> Some t2
      | _ -> None)
