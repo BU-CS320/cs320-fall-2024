19fcd4d0d3e9c85f14bdebe4ed357daa7137dcd3
Nathan Mull <nmmull@gmail.com>
2024-10-24 19:31:40 -0400

    added code from today

--- lectures/10-24/parser_demo/lib/utils/utils.ml
+++ lectures/10-24/parser_demo/lib/utils/utils.ml
@@ -9,6 +9,8 @@ type expr =
   | Var of string
   | Num of int
   | Let of string * expr * expr
+  | LetFun of string * string * expr * expr
+  | App of expr * expr
   | Bop of bop * expr * expr
 
 type prog = expr
