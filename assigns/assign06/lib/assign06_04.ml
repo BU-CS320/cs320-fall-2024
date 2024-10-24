6e4d9a36e51e8d01397b1adb4fc06d0f25d49755
Ethanyoy <119466937+Ethanyoy@users.noreply.github.com>
2024-10-23 22:08:44 -0400

    Update assign06_04.ml

--- assigns/assign06/lib/assign06_04.ml
+++ assigns/assign06/lib/assign06_04.ml
@@ -1,26 +1,16 @@
-type value = 
-  | VNum of int
-  | VBool of bool
-
-type expr = 
-  | Num of int
-  | Add of expr * expr
-  | Lt of expr * expr
-  | Ite of expr * expr * expr
-
-let rec eval (e: expr) : value =
+let rec eval (e: Utils.expr) : Utils.value =
   match e with
-  | Num n -> VNum n
-  | Add(e1, e2) ->
+  | Utils.Num n -> Utils.VNum n
+  | Utils.Add(e1, e2) ->
     (match (eval e1, eval e2) with
-     | (VNum v1, VNum v2) -> VNum (v1 + v2)
+     | (Utils.VNum v1, Utils.VNum v2) -> Utils.VNum (v1 + v2)
      | _ -> failwith "Type error")
-  | Lt(e1, e2) ->
+  | Utils.Lt(e1, e2) ->
     (match (eval e1, eval e2) with
-     | (VNum v1, VNum v2) -> VBool (v1 < v2)
+     | (Utils.VNum v1, Utils.VNum v2) -> Utils.VBool (v1 < v2)
      | _ -> failwith "Type error")
-  | Ite(e1, e2, e3) ->
+  | Utils.Ite(e1, e2, e3) ->
     (match eval e1 with
-     | VBool true -> eval e2
-     | VBool false -> eval e3
+     | Utils.VBool true -> eval e2
+     | Utils.VBool false -> eval e3
      | _ -> failwith "Type error")
