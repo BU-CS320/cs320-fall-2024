7d35c42cd05cb59fdccee0eb50406e1161806ffd
Nathan Mull <nmmull@gmail.com>
2024-10-24 10:23:01 -0400

    minor change to starter

--- lectures/10-24/parser_demo/lib/utils/utils.ml
+++ lectures/10-24/parser_demo/lib/utils/utils.ml
@@ -1,12 +1,14 @@
 type bop =
   | Add
+  | Sub
   | Mul
+  | Div
+
 
 type expr =
   | Var of string
   | Num of int
   | Let of string * expr * expr
-  | App of expr * expr
   | Bop of bop * expr * expr
 
 type prog = expr
