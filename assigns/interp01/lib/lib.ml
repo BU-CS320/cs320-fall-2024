cbfb1470df20ae2f596b27084922e86ac5a6a161
Ethanyoy <119466937+Ethanyoy@users.noreply.github.com>
2024-11-09 17:51:53 -0500

    Update lib.ml

--- assigns/interp01/lib/lib.ml
+++ assigns/interp01/lib/lib.ml
@@ -17,55 +17,60 @@ let value_to_expr = function
 
 (* Substitutes v (of type value) for variable x in expression e *)
 let rec subst v x expr =
+  let v_expr = match v with
+    | VNum n -> Num n
+    | VBool b -> if b then True else False
+    | VUnit -> Unit
+    | VFun (arg, body) -> Fun (arg, body)
+  in
   match expr with
-  | Var y -> if y = x then v else Var y  (* Replace variable if it matches *)
-  | Num n -> Num n  (* Numbers are not affected by substitution *)
-  | Unit -> Unit
-  | True -> True
-  | False -> False
-  | If (e1, e2, e3) -> If (subst v x e1, subst v x e2, subst v x e3)  (* Substitute in all branches of if *)
+  | Var y -> if y = x then v_expr else Var y
+  | Num _ | Unit | True | False -> expr
+  | If (e1, e2, e3) -> If (subst v x e1, subst v x e2, subst v x e3)
   | Let (y, e1, e2) ->
-      if y = x then Let (y, subst v x e1, e2)  (* Do not substitute in e2 if y = x *)
-      else Let (y, subst v x e1, subst v x e2)
+      if y = x then Let (y, subst v x e1, e2) else Let (y, subst v x e1, subst v x e2)
   | Fun (y, e_body) ->
-      if y = x then Fun (y, e_body)  (* Avoid substitution within function body if variable is shadowed *)
-      else Fun (y, subst v x e_body)
-  | App (e1, e2) -> App (subst v x e1, subst v x e2)  (* Substitute in both function and argument *)
-  | Bop (op, e1, e2) -> Bop (op, subst v x e1, subst v x e2)  (* Substitute in binary operations *)
+      if y = x then Fun (y, e_body) else Fun (y, subst v x e_body)
+  | App (e1, e2) -> App (subst v x e1, subst v x e2)
+  | Bop (op, e1, e2) -> Bop (op, subst v x e1, subst v x e2)
 
 
 (* Evaluates expressions and returns a result or an error *)
 let rec eval expr =
   match expr with
-  | Num n -> Ok (VNum n)  (* Numbers evaluate to themselves *)
+  | Num n -> Ok (VNum n)
   | True -> Ok (VBool true)
   | False -> Ok (VBool false)
   | Unit -> Ok VUnit
-  | Var x -> Error (UnknownVar x)  (* Return error for unbound variables *)
-
-  | If (cond, e_then, e_else) ->
-      (match eval cond with
-       | Ok (VBool true) -> eval e_then
-       | Ok (VBool false) -> eval e_else
-       | Ok _ -> Error InvalidIfCond
-       | Error err -> Error err)
-
-  | Let (x, e1, e2) ->
-      (match eval e1 with
-       | Ok v -> eval (subst (value_to_expr v) x e2)  (* Substitute value in e2 *)
-       | Error err -> Error err)
-
-  | Fun (x, e_body) -> Ok (VFun (x, e_body))  (* Functions evaluate to themselves *)
-
-  | App (e1, e2) ->
-      (match eval e1 with
-       | Ok (VFun (x, e_body)) -> (
-           match eval e2 with
-           | Ok v -> eval (subst (value_to_expr v) x e_body)  (* Apply substitution and evaluate *)
-           | Error err -> Error err)
-       | Ok _ -> Error InvalidApp
-       | Error err -> Error err)
-
+  | Var x -> Error (UnknownVar x)
+  
+  | If (cond, e_then, e_else) -> (
+      match eval cond with
+      | Ok (VBool true) -> eval e_then
+      | Ok (VBool false) -> eval e_else
+      | Ok _ -> Error InvalidIfCond
+      | Error err -> Error err
+    )
+  
+  | Let (x, e1, e2) -> (
+      match eval e1 with
+      | Ok v -> eval (subst v x e2)
+      | Error err -> Error err
+    )
+  
+  | Fun (x, e_body) -> Ok (VFun (x, e_body))
+  
+  | App (e1, e2) -> (
+      match eval e1 with
+      | Ok (VFun (x, e_body)) -> (
+          match eval e2 with
+          | Ok v -> eval (subst v x e_body)
+          | Error err -> Error err
+        )
+      | Ok _ -> Error InvalidApp
+      | Error err -> Error err
+    )
+  
   | Bop (op, e1, e2) ->
       let apply_bop op v1 v2 =
         match op with
@@ -80,23 +85,21 @@ let rec eval expr =
         | Gte -> Ok (VBool (v1 >= v2))
         | Eq -> Ok (VBool (v1 = v2))
         | Neq -> Ok (VBool (v1 <> v2))
-        | _ -> Error (InvalidArgs Add)  (* Replace 'Add' with a specific operator or error context *)
-
+        | _ -> Error (InvalidArgs op)
       in
       (match eval e1, eval e2 with
-       | Ok (VNum v1), Ok (VNum v2) -> apply_bop op v1 v2  (* Arithmetic and comparison operators *)
-       | Ok (VBool b1), Ok (VBool b2) -> (
-           match op with
-           | And -> Ok (VBool (b1 && b2))
-           | Or -> Ok (VBool (b1 || b2))
-           | _ -> Error InvalidArgs)
-       | _ -> Error InvalidArgs)  (* Error for incompatible types *)
-
+      | Ok (VNum v1), Ok (VNum v2) -> apply_bop op v1 v2
+      | Ok (VBool b1), Ok (VBool b2) -> (
+          match op with
+          | And -> Ok (VBool (b1 && b2))
+          | Or -> Ok (VBool (b1 || b2))
+          | _ -> Error (InvalidArgs op)
+        )
+      | _ -> Error (InvalidArgs op))
 
 
 (* Combines parsing and evaluation *)
 let interp s =
   match parse s with
   | Some expr -> eval expr
-  | None -> Error ParseFail  (* Return error if parsing fails *)
-
+  | None -> Error ParseFail
