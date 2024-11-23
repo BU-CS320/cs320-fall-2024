dabff45d8be603a983715aff5b6ab6f837fec7a4
Ethanyoy <119466937+Ethanyoy@users.noreply.github.com>
2024-11-23 17:05:09 -0500

    Update utils.ml

--- assigns/interp02/lib/utils/utils.ml
+++ assigns/interp02/lib/utils/utils.ml
@@ -1,32 +1,60 @@
-type ty =
-  | IntTy
-  | BoolTy
-  | UnitTy
-  | FunTy of ty * ty
-
 type bop =
   | Add | Sub | Mul | Div | Mod
   | Lt | Lte | Gt | Gte | Eq | Neq
   | And | Or
 
-type sfexpr =
-  | SUnit
-  | STrue
-  | SFalse
-  | SNum of int
-  | SVar of string
-  | SFun of { arg : string * ty; args : (string * ty) list; body : sfexpr }
-  | SApp of sfexpr * sfexpr
-  | SLet of { is_rec : bool; name : string; args : (string * ty) list; ty : ty; value : sfexpr; body : sfexpr }
-  | SIf of sfexpr * sfexpr * sfexpr
-  | SBop of bop * sfexpr * sfexpr
+type expr =
+  | Num of int
+  | Var of string
+  | Unit | True | False
+  | App of expr * expr
+  | Bop of bop * expr * expr
+  | If of expr * expr * expr
+  | Let of string * expr * expr
+  | Fun of string * expr
 
-type toplet =
-  { is_rec : bool
-  ; name : string
-  ; args : (string * ty) list
-  ; ty : ty
-  ; value : sfexpr
-  }
+type prog = expr
 
-type prog = toplet list
+type value =
+  | VNum of int
+  | VBool of bool
+  | VUnit
+  | VFun of string * expr
+
+type error =
+  | DivByZero
+  | InvalidIfCond
+  | InvalidArgs of bop
+  | InvalidApp
+  | UnknownVar of string
+  | ParseFail
+
+let string_of_value = function
+  | VNum n -> string_of_int n
+  | VBool true -> "true"
+  | VBool false -> "false"
+  | VUnit -> "()"
+  | VFun (_,_) -> "<fun>"
+
+let string_of_bop = function
+  | Add -> "(+)"
+  | Sub -> "(-)"
+  | Mul -> "(*)"
+  | Div -> "(/)"
+  | Mod -> "(mod)"
+  | Lt -> "(<)"
+  | Lte -> "(<=)"
+  | Gt -> "(>)"
+  | Gte -> "(>=)"
+  | Eq -> "(=)"
+  | Neq -> "(<>)"
+  | And -> "(&&)"
+  | Or -> "(||)"
+
+let err_msg = function
+  | DivByZero -> "division by zero"
+  | InvalidIfCond -> "non-Boolean value given as condition"
+  | InvalidArgs op -> "invalid arguments given to " ^ string_of_bop op
+  | InvalidApp -> "non-function value used in function application"
+  | UnknownVar x -> "unknown variable \'" ^ x ^ "\'"
+  | ParseFail -> "syntax error"
