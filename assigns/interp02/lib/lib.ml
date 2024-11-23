21a11c565f4103b059b52f9f5ccd8c96cbeeeed7
Ethanyoy <119466937+Ethanyoy@users.noreply.github.com>
2024-11-23 17:07:06 -0500

    Update lib.ml

--- assigns/interp02/lib/lib.ml
+++ assigns/interp02/lib/lib.ml
@@ -1,47 +1,142 @@
 include Utils
+
 open My_parser
 
+(* Parsing function *)
 let parse s =
   try Some (Par.prog Lex.read (Lexing.from_string s))
   with _ -> None
 
-let rec desugar_expr = function
-  | SUnit -> Unit
-  | STrue -> True
-  | SFalse -> False
-  | SNum n -> Num n
-  | SVar x -> Var x
-  | SFun { arg = (x, ty); args; body } ->
-      List.fold_right (fun (x, ty) acc -> Fun (x, ty, acc)) ((x, ty) :: args) (desugar_expr body)
-  | SApp (e1, e2) -> App (desugar_expr e1, desugar_expr e2)
-  | SLet { is_rec; name; args; ty; value; body } ->
-      Let { is_rec; name; ty; value = desugar_expr value; body = desugar_expr body }
-  | SIf (cond, e_then, e_else) -> If (desugar_expr cond, desugar_expr e_then, desugar_expr e_else)
-  | SBop (op, e1, e2) -> Bop (op, desugar_expr e1, desugar_expr e2)
+(* Desugaring function *)
+let rec desugar (prog: prog): expr =
+  match prog with
+  | [] -> Unit
+  | { is_rec; name; args; ty; value } :: rest ->
+    let func_expr = List.fold_right (fun (arg, ty) acc -> Fun (arg, ty, acc)) args value in
+    let desugared_rest = desugar rest in
+    if is_rec then
+      Let { is_rec = true; name; ty; value = func_expr; body = desugared_rest }
+    else
+      Let { is_rec = false; name; ty; value = func_expr; body = desugared_rest }
 
-let desugar prog =
-  List.fold_right
-    (fun toplet acc ->
-       Let
-         { is_rec = toplet.is_rec
-         ; name = toplet.name
-         ; ty = toplet.ty
-         ; value = desugar_expr toplet.value
-         ; body = acc
-         })
-    prog
-    Unit
-
-let rec type_of ctx expr = (* Existing logic here *)
-
-let rec eval expr = (* Existing logic here *)
-
-let interp s =
-  match parse s with
-  | Some prog -> (
-      let expr = desugar prog in
-      match type_of [] expr with
-      | Ok _ -> eval expr
+(* Type checking function *)
+let rec type_of ctx expr =
+  match expr with
+  | Num _ -> Ok IntTy
+  | True | False -> Ok BoolTy
+  | Unit -> Ok UnitTy
+  | Var x -> (
+      match List.assoc_opt x ctx with
+      | Some t -> Ok t
+      | None -> Error (UnknownVar x)
+    )
+  | If (cond, e_then, e_else) -> (
+      match type_of ctx cond with
+      | Ok BoolTy -> (
+          match type_of ctx e_then, type_of ctx e_else with
+          | Ok t1, Ok t2 when t1 = t2 -> Ok t1
+          | Ok t1, Ok t2 -> Error (IfTyErr (t1, t2))
+          | _, Error e -> Error e
+        )
+      | Ok t -> Error (IfCondTyErr t)
       | Error e -> Error e
     )
+  | Fun (x, ty, e_body) ->
+      let extended_ctx = (x, ty) :: ctx in
+      (match type_of extended_ctx e_body with
+      | Ok t_body -> Ok (FunTy (ty, t_body))
+      | Error e -> Error e)
+  | App (e1, e2) -> (
+      match type_of ctx e1, type_of ctx e2 with
+      | Ok (FunTy (t_arg, t_ret)), Ok t when t_arg = t -> Ok t_ret
+      | Ok (FunTy (t_arg, _)), Ok t -> Error (FunArgTyErr (t_arg, t))
+      | Ok t, _ -> Error (FunAppTyErr t)
+      | Error e, _ -> Error e
+    )
+  | Let { is_rec; name; ty; value; body } ->
+      let extended_ctx = if is_rec then (name, ty) :: ctx else ctx in
+      (match type_of extended_ctx value with
+      | Ok t_value when t_value = ty ->
+          type_of ((name, ty) :: ctx) body
+      | Ok t_value -> Error (LetTyErr (ty, t_value))
+      | Error e -> Error e)
+  | Assert e -> (
+      match type_of ctx e with
+      | Ok BoolTy -> Ok UnitTy
+      | Ok t -> Error (AssertTyErr t)
+      | Error e -> Error e
+    )
+  | Bop (op, e1, e2) ->
+      (* Check binary operator type *)
+      let check_bop t1 t2 expected_ty =
+        match t1, t2 with
+        | Ok t1', Ok t2' when t1' = expected_ty && t2' = expected_ty -> Ok expected_ty
+        | Ok t1', Ok t2' when t1' <> expected_ty -> Error (OpTyErrL (op, expected_ty, t1'))
+        | Ok _, Ok t2' -> Error (OpTyErrR (op, expected_ty, t2'))
+        | Error e, _ -> Error e
+        | _, Error e -> Error e
+      in
+      (match op with
+      | Add | Sub | Mul | Div | Mod -> check_bop (type_of ctx e1) (type_of ctx e2) IntTy
+      | Lt | Lte | Gt | Gte -> check_bop (type_of ctx e1) (type_of ctx e2) IntTy
+      | Eq | Neq -> check_bop (type_of ctx e1) (type_of ctx e2) IntTy
+      | And | Or -> check_bop (type_of ctx e1) (type_of ctx e2) BoolTy)
+
+(* Evaluation function *)
+let rec eval env expr =
+  match expr with
+  | Num n -> VNum n
+  | True -> VBool true
+  | False -> VBool false
+  | Unit -> VUnit
+  | Var x -> (
+      match List.assoc_opt x env with
+      | Some v -> v
+      | None -> raise (UnknownVar x)
+    )
+  | If (cond, e_then, e_else) -> (
+      match eval env cond with
+      | VBool true -> eval env e_then
+      | VBool false -> eval env e_else
+      | _ -> raise InvalidIfCond
+    )
+  | Fun (x, _, e_body) -> VClos (env, x, e_body)
+  | App (e1, e2) -> (
+      match eval env e1 with
+      | VClos (env', x, e_body) ->
+          let v_arg = eval env e2 in
+          eval ((x, v_arg) :: env') e_body
+      | _ -> raise InvalidApp
+    )
+  | Let { is_rec; name; value; body } ->
+      let v = if is_rec then
+        let rec_env = (name, ref VUnit) :: env in
+        let v = eval rec_env value in
+        (match List.assoc_opt name rec_env with
+        | Some r -> r := v; v
+        | None -> failwith "Unexpected missing recursive binding")
+      else eval env value in
+      eval ((name, v) :: env) body
+  | Assert e -> (
+      match eval env e with
+      | VBool true -> VUnit
+      | VBool false -> raise AssertFail
+      | _ -> raise InvalidIfCond
+    )
+  | Bop (op, e1, e2) -> eval_binary_op op (eval env e1) (eval env e2)
+
+(* Top-level interpreter *)
+let interp s =
+  match parse s with
   | None -> Error ParseFail
+  | Some prog -> (
+      try
+        let expr = desugar prog in
+        match type_of [] expr with
+        | Ok _ -> Ok (eval [] expr)
+        | Error e -> Error e
+      with
+      | DivByZero -> Error DivByZero
+      | AssertFail -> Error AssertFail
+      | e -> failwith (Printexc.to_string e)
+    )
