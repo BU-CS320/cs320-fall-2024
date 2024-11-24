8bfe20e8ac450cad06f683f68bf1f31751b0bb4f
Ethanyoy <119466937+Ethanyoy@users.noreply.github.com>
2024-11-23 23:36:08 -0500

    Update utils.ml

--- assigns/interp02/lib/utils/utils.ml
+++ assigns/interp02/lib/utils/utils.ml
@@ -19,65 +19,8 @@ type sfexpr =
 and sftoplet =
   | SToplet of { is_rec: bool; name: string; args: (string * ty) list; ty: ty; value: sfexpr }
 
-(* Core language expressions (used after desugaring) *)
-type expr =
-  | Num of int
-  | Var of string
-  | Unit | True | False
-  | App of expr * expr
-  | Bop of bop * expr * expr
-  | If of expr * expr * expr
-  | Let of { is_rec: bool; name: string; ty: ty; value: expr; body: expr }
-  | Fun of string * ty * expr
-  | Assert of expr
-
 type ty =
   | IntTy
   | BoolTy
   | UnitTy
   | FunTy of ty * ty
-
-type value =
-  | VNum of int
-  | VBool of bool
-  | VUnit
-  | VFun of string * expr
-
-type error =
-  | DivByZero
-  | InvalidIfCond
-  | InvalidArgs of bop
-  | InvalidApp
-  | UnknownVar of string
-  | ParseFail
-
-let string_of_value = function
-  | VNum n -> string_of_int n
-  | VBool true -> "true"
-  | VBool false -> "false"
-  | VUnit -> "()"
-  | VFun (_, _) -> "<fun>"
-
-let string_of_bop = function
-  | Add -> "(+)"
-  | Sub -> "(-)"
-  | Mul -> "(*)"
-  | Div -> "(/)"
-  | Mod -> "(mod)"
-  | Lt -> "(<)"
-  | Lte -> "(<=)"
-  | Gt -> "(>)"
-  | Gte -> "(>=)"
-  | Eq -> "(=)"
-  | Neq -> "(<>)"
-  | And -> "(&&)"
-  | Or -> "(||)"
-
-let err_msg = function
-  | DivByZero -> "division by zero"
-  | InvalidIfCond -> "non-Boolean value given as condition"
-  | InvalidArgs op -> "invalid arguments given to " ^ string_of_bop op
-  | InvalidApp -> "non-function value used in function application"
-  | UnknownVar x -> "unknown variable '" ^ x ^ "'"
-  | ParseFail -> "syntax error"
-
